7fd0713101600202b74074c8e1f6c7457538290b
unknown <luvuyotafeni012@gmail.com>
2026-09-08 12:19:55 +0200

    api gateway configs

---
+++ api-gateway-b4/src/main/java/com/example/api_gateway_b4/ApiGatewayB4Application.java
@@ -0,0 +1,13 @@
+package com.example.api_gateway_b4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayB4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayB4Application.class, args);
+	}
+
+}
